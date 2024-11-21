daa2efc3c2d7eccc0237b87466303e1a45f696cf
hzorrilla <152939685+hzorrilla@users.noreply.github.com>
2024-11-20 19:50:07 -0500

    dark mode styles added to notification and sales list

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -49,7 +49,7 @@ const Home = () => {
   };
 
   return (
-    <div className="p-8">
+    <div className="p-8 dark:text-white">
       <h1 className="text-2xl font-bold mb-4">Dashboard de Ventas</h1>
 
       <div className="grid grid-cols-2 gap-8">
--- components/dashboard/SalesList.tsx
+++ components/dashboard/SalesList.tsx
@@ -14,23 +14,23 @@ interface SalesListProps {
 
 const SalesList: React.FC<SalesListProps> = ({ salesData }) => {
   return (
-    <div className="mt-8 bg-white shadow-sm rounded-md p-6">
+    <div className="mt-8 bg-white shadow-sm rounded-md p-6 dark:bg-black">
       <h2 className="text-xl font-semibold mb-4">Últimas Ventas</h2>
       <table className="min-w-full table-auto">
-        <thead className="bg-gray-100">
+        <thead className="bg-gray-100 dark:bg-slate-600">
           <tr>
-            <th className="px-4 py-2 text-left text-sm font-medium text-gray-600">Producto</th>
-            <th className="px-4 py-2 text-left text-sm font-medium text-gray-600">Fecha</th>
-            <th className="px-4 py-2 text-left text-sm font-medium text-gray-600">Monto</th>
-            <th className="px-4 py-2 text-left text-sm font-medium text-gray-600">Thumbnail</th>
+            <th className="px-4 py-2 text-left text-sm font-medium text-gray-600 dark:text-white">Producto</th>
+            <th className="px-4 py-2 text-left text-sm font-medium text-gray-600 dark:text-white">Fecha</th>
+            <th className="px-4 py-2 text-left text-sm font-medium text-gray-600 dark:text-white">Monto</th>
+            <th className="px-4 py-2 text-left text-sm font-medium text-gray-600 dark:text-white">Thumbnail</th>
           </tr>
         </thead>
         <tbody>
           {salesData.map((item) => (
-            <tr key={item.id} className="border-b hover:bg-gray-50">
-              <td className="px-4 py-2 text-sm text-gray-700">{item.title}</td>
-              <td className="px-4 py-2 text-sm text-gray-700">{item.date}</td>
-              <td className="px-4 py-2 text-sm font-semibold text-gray-900">${item.amount}</td>
+            <tr key={item.id} className="border-b hover:bg-gray-50 dark:hover:bg-slate-500">
+              <td className="px-4 py-2 text-sm text-gray-700 dark:text-white">{item.title}</td>
+              <td className="px-4 py-2 text-sm text-gray-700 dark:text-white">{item.date}</td>
+              <td className="px-4 py-2 text-sm font-semibold text-gray-900 dark:text-white">${item.amount}</td>
               <td className="px-4 py-2">
                 <img
                   src={item.thumbnail}
