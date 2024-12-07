dd72c3b34d03a7b0e2c7ff46630b7469cdb72369
Hillari Z <hillarizorrilla@gmail.com>
2024-12-07 15:02:40 -0500

    chore(dependencies): install webpack-bundle-analyzer and cross-env
    Added webpack-bundle-analyzer for analyzing bundles and cross-env for cross-platform environment variable management.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const config = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+  });
 
 const nextConfig: NextConfig = {
     env: {
         NEXT_PUBLIC_GRAPHQL_API: process.env.NEXT_PUBLIC_GRAPHQL_API,
         PORT: process.env.PORT,
     },
+    webpack(config) {
+        // Modificaciones adicionales de Webpack si son necesarias
+        return config;
+    },
 };
 
-export default nextConfig;
+export default config(nextConfig);
